c611ab97b0c77f3e0617c289df9c97bdd97ac1c4
Roman Fechtchenko <roman1.fechtchenko@epitech.eu>
2022-10-25 14:49:35 +0200

    add USERS METHOD + CLOCK METHODS

--- api/lib/api_web/controllers/clock_controller.ex
+++ api/lib/api_web/controllers/clock_controller.ex
@@ -11,8 +11,9 @@ defmodule ApiWeb.ClockController do
     render(conn, "index.json", clocks: clocks)
   end
 
-  def create(conn, %{"clock" => clock_params}) do
-    with {:ok, %Clock{} = clock} <- Clocks.create_clock(clock_params) do
+  def create(conn, %{"userId" => id,"clock" => clock_params}) do
+    completed_params = Map.put(clock_params, "user", id)
+    with {:ok, %Clock{} = clock} <- Clocks.create_clock(completed_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.clock_path(conn, :show, clock))
@@ -25,19 +26,19 @@ defmodule ApiWeb.ClockController do
     render(conn, "show.json", clock: clock)
   end
 
-  def update(conn, %{"id" => id, "clock" => clock_params}) do
-    clock = Clocks.get_clock!(id)
-
-    with {:ok, %Clock{} = clock} <- Clocks.update_clock(clock, clock_params) do
-      render(conn, "show.json", clock: clock)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    clock = Clocks.get_clock!(id)
-
-    with {:ok, %Clock{}} <- Clocks.delete_clock(clock) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+#  def update(conn, %{"id" => id, "clock" => clock_params}) do
+#    clock = Clocks.get_clock!(id)
+#
+#    with {:ok, %Clock{} = clock} <- Clocks.update_clock(clock, clock_params) do
+#      render(conn, "show.json", clock: clock)
+#    end
+#  end
+#
+#  def delete(conn, %{"id" => id}) do
+#    clock = Clocks.get_clock!(id)
+#
+#    with {:ok, %Clock{}} <- Clocks.delete_clock(clock) do
+#      send_resp(conn, :no_content, "")
+#    end
+#  end
 end
