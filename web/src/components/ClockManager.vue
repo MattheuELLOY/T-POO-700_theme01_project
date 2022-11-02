d62aa69accca610da0b24dab0500b3f8870286e7
Roman Fechtchenko <roman1.fechtchenko@epitech.eu>
2022-11-02 10:21:11 +0100

    feat: tailwind lib

--- web/src/components/ClockManager.vue
+++ web/src/components/ClockManager.vue
@@ -16,7 +16,19 @@
     Create Clock
   </button>
   </div>
+  <label v-if="clocks.status==true" for="yellow-toggle" class="inline-flex relative items-center mr-5 cursor-pointer">
+    <input @click="createClock" type="checkbox" value="{{clocks.status}}" id="yellow-toggle" class="sr-only peer" checked>
+    <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-300 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-400"></div>
+    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Active</span>
+  </label>
+
+  <label v-if="clocks.status!=true" for="yellow-toggle" class="inline-flex relative items-center mr-5 cursor-pointer">
+    <input @click="createClock" type="checkbox" value="{{clocks.status}}" id="yellow-toggle" class="sr-only peer" >
+    <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-300 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-400"></div>
+    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Inactive</span>
+  </label>
 </template>
+
 
 <script>
 
@@ -46,6 +58,7 @@ export default {
       function onClick() {
         createClock()
         console.log(props.userId)
+        console.log(users)
       };
       function createClock() {
         createClockByUserId(props.userId).then(() => clockStore.getClockByUserId(props.userId))
@@ -59,6 +72,8 @@ export default {
     }
 }
 </script>
+
+
 <style>
 .content {
   min-width: 30rem;
--- web/src/main.ts
+++ web/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import './assets/main.css'
 import router from '@/router'
+import '@/style.css';
 
 createApp(App).use(router).use(createPinia()).mount('#app')
